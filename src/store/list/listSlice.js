61abed9d322178ec63048ea88d3b895892dc19b3
choyj109 <choyj109@gmail.com>
2022-08-30 23:21:29 +0900

    todo sample

---
+++ src/store/list/listSlice.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getList = createAsyncThunk("GET_LIST", async () => {
+  try {
+    const res = await axios.get("http://localhost:5005/list");
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export const addList = createAsyncThunk("ADD_LIST", async (newList) => {
+  try {
+    const res = await axios.post(`http://localhost:5005/list`, newList);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export const deleteList = createAsyncThunk("DELETE_LIST", async (id) => {
+  try {
+    const res = await axios.delete(`http://localhost:5005/list/${id}`);
+    return id;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export const updateList = createAsyncThunk(
+  "UPDATE_LIST",
+  async ({ id, content }) => {
+    try {
+      const res = await axios.put(`http://localhost:5005/list/${id}`, {
+        content: content,
+      });
+      return { id, content };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+const listSlice = createSlice({
+  name: "list",
+  initialState: {
+    data: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getList.fulfilled, (state, action) => {
+      state.data = action.payload;
+    });
+    builder.addCase(addList.fulfilled, (state, action) => {
+      state.data.push(action.payload);
+    });
+    builder.addCase(deleteList.fulfilled, (state, action) => {
+      state.data = state.data.filter((ele) => ele.id !== action.payload);
+    });
+    builder.addCase(updateList.fulfilled, (state, action) => {
+      const idx = state.data.findIndex((ele) => ele.id === action.payload.id);
+      state.data.splice(idx, 1, action.payload);
+    });
+  },
+});
+
+export default listSlice.reducer;
